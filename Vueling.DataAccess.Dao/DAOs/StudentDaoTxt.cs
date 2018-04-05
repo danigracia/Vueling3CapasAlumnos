092933ba5f02a02cc0c70bfd9edd29dce6266d7f
E <epr1122@hotmail.com>
2018-04-05 15:53:10 +0200

    Transportar de StudentDaoTxt to FileUtils.

--- Vueling.DataAccess.Dao/DAOs/StudentDaoTxt.cs
+++ Vueling.DataAccess.Dao/DAOs/StudentDaoTxt.cs
@@ -28,55 +28,5 @@ namespace Vueling.DataAccess.Dao
             return FileUtils.GetStudentFromTxtByGuid(student.Student_Guid, path);
         }
 
-
-        private void SetStudentToTxt(Student student, string path)
-        {
-            log.Info("Metodo " + System.Reflection.MethodBase.GetCurrentMethod().Name +
-                " iniciado");
-            if (!File.Exists(path))
-            {
-                using (StreamWriter stwriter = File.CreateText(path))
-                {
-                    stwriter.WriteLine(student.ToString());
-                }
-            }
-            else
-            {
-                using (StreamWriter strw = File.AppendText(path))
-                {
-                    strw.WriteLine(student.ToString());
-                }
-                //File.AppendAllText(path, student.ToString() + Environment.NewLine);
-            }
-            log.Info("Metodo " + System.Reflection.MethodBase.GetCurrentMethod().Name +
-                " terminado");
-
-        }
-
-        private Student GetStudentFromTxtByGuid(Guid studentguid, string path)
-        {
-            log.Info("Metodo " + System.Reflection.MethodBase.GetCurrentMethod().Name +
-                " iniciado");
-            var alllines = File.ReadAllLines(path);
-            string findstudent = "";
-            foreach (string line in alllines)
-            {
-                if (line.Contains(studentguid.ToString()))
-                {
-                    findstudent = line;
-                } 
-            }
-
-            var linesplit = findstudent.Split(',');
-            Student readstudent = new Student(Int32.Parse(linesplit[0]), linesplit[1], linesplit[2], Int32.Parse(linesplit[3]), linesplit[4], linesplit[5], linesplit[6]);
-
-
-            log.Info("Metodo " + System.Reflection.MethodBase.GetCurrentMethod().Name +
-                " terminado");
-            log.Info("Datos del student leido del file txt:");
-            log.Info("datebirth:" + readstudent.FechaNacimiento.ToString());
-            return readstudent;
-        }
-
     }
 }
